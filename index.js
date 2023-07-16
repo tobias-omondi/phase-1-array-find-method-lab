a3b46221ba12617d2f757eba1768e2342574caeb
tobias <tobias.omondi@student.moringaschool.com>
2023-07-16 22:04:47 +0300

    my-assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(array){
+    const winGame = array.find((game) => game.result === "W");
+
+  if (winGame) {
+    return winGame.year;
+  } else {
+    return undefined;
+  }
+}
